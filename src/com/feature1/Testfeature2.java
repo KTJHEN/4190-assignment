faf72ec86153a693a27b4cf54f155210d507039e
houji <houjietangken@gmail.com>
2024-11-17 19:23:33 -0800

    Test feature2

--- src/com/feature1/Testfeature2.java
+++ src/com/feature1/Testfeature2.java
@@ -1,14 +1,29 @@
 package com.feature1;
-
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;
-
 class Testfeature2 {
+    @Test
+    void testMultiInputs() {
+        String[] input = {"a", "c", "b", "g", "z"};
+        String[] expected = {"a", "b", "c", "g", "z"};
+        assertArrayEquals(expected, feature2.sortStrings(input));
+    }
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+    @Test
+    void testNoInput() {
+        String[] input = {};
+        String[] expected = {};
+        assertArrayEquals(expected, feature2.sortStrings(input));
+    }
+
+    @Test
+    void testSingleInput() {
+        String[] input = {"a"};
+        String[] expected = {"a"};
+        assertArrayEquals(expected, feature2.sortStrings(input));
+    }
+
 
 }
+
