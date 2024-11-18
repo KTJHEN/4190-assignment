c076944d9defb4518c48ca91a92a24a348421ccd
houji <houjietangken@gmail.com>
2024-11-17 18:42:34 -0800

    adding testfile in feature2

---
+++ src/com/feature1/Testfeature2.java
@@ -0,0 +1,14 @@
+package com.feature1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Testfeature2 {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
